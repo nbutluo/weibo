1fd5d93d04f489822c9bba291ddc54a3bf3c841e
nbutluo <nbutluo@163.com>
2021-02-17 13:52:09 +0800

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,8 +8,12 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+
+                @include('shared._error')
+
                 <form method="POST" action="{{ route('users.store') }}">
                     @csrf
+
                     <div class="form-group">
                         <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
