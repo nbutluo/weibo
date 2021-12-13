50e7531704b04cf1f963e73baabcdcca7ee4cd9f
nbutluo <nbutluo@163.com>
2021-12-13 18:35:37 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\StaticPagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // Route::get('/about', [StaticPagesController::class, 'about']);
