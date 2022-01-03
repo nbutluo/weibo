c6131e68bc1bc44a25b5aa4a50526fe29c257eab
nbutluo <nbutluo@163.com>
2022-01-03 19:16:38 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -14,5 +15,16 @@ class UserController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users,name|max:25',
+            'email' => 'required|email|unique:users,email|max:50',
+            'password' => 'required|confirmed|max:50',
+        ]);
+
+        return;
     }
 }
