770f7f1789d36ff994b6b808f551c5b48df71ba9
nbutluo <nbutluo@163.com>
2022-01-05 15:52:47 +0800

    查看用户列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // Model::unguard();
+
+        $this->call([
+            UsersTableSeeder::class,
+        ]);
+        Model::unguard();
     }
 }
