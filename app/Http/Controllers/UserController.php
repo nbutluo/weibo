3e1489d169cb5442b16fbf31f0cfae68c1aa24e0
nbutluo <nbutluo@163.com>
2022-01-12 10:16:05 +0800

    关注和粉丝列表页面

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -127,4 +127,20 @@ class UserController extends Controller
 
         return back()->with('success', '删除成功');
     }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title =  $user->name . '关注的人';
+
+        return view('users.show_follow', compact('title', 'users'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate();
+        $title =  $user->name . '的粉丝';
+
+        return view('users.show_follow', compact('title', 'users'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 
 Route::resource('statuses', 'StatusesController')->only(['store', 'destroy']);
+
+Route::get('/users/{user}/followings', 'UserController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UserController@followers')->name('users.followers');
